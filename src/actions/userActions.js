818b0f68706535c93f618e2b9c10d27c7fe0b136
khanh <khanhguitar96@gmail.com>
2022-07-20 20:12:40 +0700

    Fe-Project

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -10,6 +10,7 @@ import {
   USER_LIST_FAIL,
   USER_LIST_REQUEST,
   USER_LIST_SUCCESS,
+  USER_LIST_RESET,
   USER_LOGIN_FAIL,
   USER_LOGIN_REQUEST,
   USER_LOGIN_SUCCESS,
@@ -63,9 +64,9 @@ export const logout = () => (dispatch) => {
   //   localStorage.removeItem('shippingAddress')
   //   localStorage.removeItem('paymentMethod')
   dispatch({ type: USER_LOGOUT });
-  //   dispatch({ type: USER_DETAILS_RESET })
+    dispatch({ type: USER_DETAILS_RESET })
   //   dispatch({ type: ORDER_LIST_MY_RESET })
-  //   dispatch({ type: USER_LIST_RESET })
+    dispatch({ type: USER_LIST_RESET })
   //   document.location.href = '/login'
 };
 
--- src/screens/UserListScreen.js
+++ src/screens/UserListScreen.js
@@ -6,15 +6,23 @@ import Message from "../components/Message";
 import Loader from "../components/Loader";
 import { listUsers } from "../actions/userActions";
 
-const UserListScreen = () => {
+const UserListScreen = ({history}) => {
   const dispatch = useDispatch();
 
   const userList = useSelector((state) => state.userList);
   const { loading, error, users } = userList;
 
+  
+  const userLogin = useSelector((state) => state.userLogin)
+  const { userInfo } = userLogin
+
   useEffect(() => {
-    dispatch(listUsers());
-  }, [dispatch]);
+    if (userInfo && userInfo.user.role[0].roleName === "Admin") {
+      dispatch(listUsers())
+    } else {
+      history.push('/login')
+    }
+  }, [dispatch, history]);
 
   const deleteHandler = (id) => {
     console.log("delete");
